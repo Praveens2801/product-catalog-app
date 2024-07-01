64da61fd3c57715f19f10e83a26acf3cb0e1a1b2
Praveen <praveens@wildrepublictech.com>
2024-07-01 12:35:28 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-TIXNXKJN.mjs";import"./chunk-T7D77YDG.mjs";import"./chunk-JJJETRB7.mjs";import"./chunk-A5GALVYW.mjs";import"./chunk-E4LLF52G.mjs";import"./chunk-FV7P7W2T.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
